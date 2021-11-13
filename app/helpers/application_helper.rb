c4d22ac8e5f1bdad1991aee14c1f81e6604ac028
Sheena Bonilla <sheena.myb@gmail.com>
2021-11-13 14:18:14 -0800

    Refactor helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    
+    def title(text)
+        content_for :title, text
+        #sends my text to the place in my app that is expecting content for :title
+    end
+
 end
